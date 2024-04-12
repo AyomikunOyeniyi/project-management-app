b2fcc5845ae24da229dec70a5ab305f4d010985e
Ayomikun Oyeniyi <ayomikunoyeniyi07@gmail.com>
2024-04-12 14:58:56 -0400

    Manage content state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
 import Sidebar from "./components/Sidebar";
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 
 function App() {
+  const [isAddBtnClicked, setIsAddBtnClicked] = useState(false);
+
+  function handleAddProject() {
+    setIsAddBtnClicked(true);
+  }
+
+  let content;
+
+  if (isAddBtnClicked) {
+    content = <NewProject></NewProject>
+  } else {
+    content = <NoProjectSelected onAddProject={handleAddProject}></NoProjectSelected>
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar></Sidebar>
-      <NewProject></NewProject>
+      <Sidebar onAddProject={handleAddProject}></Sidebar>
+      {content}
     </main>
   );
 }
