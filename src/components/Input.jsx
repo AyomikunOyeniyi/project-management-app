ccd9e059968d19aea7cc5b022da12fe5a4e244fb
Ayomikun Oyeniyi <ayomikunoyeniyi07@gmail.com>
2024-04-11 22:46:07 -0400

    Style components

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,10 @@
 export default function Input({inputType, isTextarea, label}) {
+    const classes = "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline:none focus:border-stone-600";
+
     return(
-        <p>
-            <label>{label}</label>
-            {isTextarea ? <textarea></textarea> : <input type={inputType}></input>}
+        <p className="flex flex-col gap-1 my-4">
+            <label className="text-sm font-bold uppercase text-stone-500">{label}</label>
+            {isTextarea ? <textarea className ={classes}></textarea> : <input type={inputType}className={classes}></input>}
         </p>
     )
 }
