094e419e362bef85b9294f5f210daeb22ecc954a
Ayomikun Oyeniyi <ayomikunoyeniyi07@gmail.com>
2024-04-14 13:11:18 -0400

    Accept onAdd prop

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -2,10 +2,23 @@ import { useRef } from "react";
 
 import Input from "./Input";
 
-export default function NewProject() {
+export default function NewProject({onAdd}) {
     const title = useRef();
     const description = useRef();
     const dueDate = useRef();
+
+    function handleSave() {
+        const enteredTitle = title.current.value;
+        const enteredDescription = description.current.value;
+        const enteredDueDate = dueDate.current.value;
+
+        onAdd({
+            title: enteredTitle,
+            description: enteredDescription,
+            dueDate: enteredDueDate
+        })
+    }
+
     return(
         <div className="w-[35rem] mt-16">
             <menu className="flex items-center justify-end gap-4 my-4">
@@ -13,14 +26,14 @@ export default function NewProject() {
                     <button className="text-stone-800 hover:text-stone-950">Cancel</button>
                 </li>
                 <li>
-                    <button className="bg-stone-800 text-stone-50 hover:bg-stone-950 px-6 py-2 rounded-md">Save</button>
+                    <button className="bg-stone-800 text-stone-50 hover:bg-stone-950 px-6 py-2 rounded-md" onClick={handleSave}>Save</button>
                 </li>
             </menu>
 
             <div>
-                <Input inputType={'text'} label={'Title'}></Input>
-                <Input inputType={'textarea'} isTextarea label={'Description'}></Input>
-                <Input inputType={'date'} label={'Due Date'}></Input>
+                <Input inputType={'text'} label={'Title'} ref={title}></Input>
+                <Input inputType={'textarea'} isTextarea label={'Description'} ref={description}></Input>
+                <Input inputType={'date'} label={'Due Date'} ref={dueDate}></Input>
             </div>
         </div>
     )
